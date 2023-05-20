49e304ced68cc73935d7ad3f4801943f1e40a7ca
Rohan Verma <86923317+DevOpRohan@users.noreply.github.com>
2023-05-20 12:00:53 +0530

    Initial commit

---
+++ src/Components/Chat/Chat.js
@@ -0,0 +1,198 @@
+import React, { useState, useEffect, useCallback, useRef } from "react";
+import {
+  Stack,
+  Text,
+  TextField,
+  getTheme,
+  mergeStyleSets
+} from "@fluentui/react";
+import BotMessage from "./BotMessage";
+import CodeFormatter from "./CodeFormatter";
+import Layout from "./Layout";
+import UserMessage from "./UserMessage";
+import MainLayout from "../MainLayout";
+import { Icon } from "@iconify/react";
+
+const Chat = () => {
+  const [userMessage, setUserMessage] = useState("");
+  const [chatMessages, setChatMessages] = useState([]);
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
+  const chatContainerRef = useRef(null);
+
+  const handleUserMessageChange = (event) => {
+    setUserMessage(event.target.value);
+  };
+
+  const handleSendMessage = () => {
+    if (userMessage) {
+      const botMessage = { message: "Typing...", isBot: true };
+      setChatMessages((prevChatMessages) => [
+        ...prevChatMessages,
+        { message: userMessage, isBot: false },
+        botMessage
+      ]);
+      setIsButtonClicked(true);
+    }
+  };
+
+  const fetchBotMessage = useCallback(() => {
+    fetch(`https://walrus-app-hodhq.ondigitalocean.app/ultron?q=${userMessage}`)
+      .then((response) => response.text())
+      .then((data) => {
+        const botMessage = { message: data, isBot: true };
+        setChatMessages((prevChatMessages) => [
+          ...prevChatMessages.slice(0, -1),
+          botMessage
+        ]);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [userMessage]);
+
+  useEffect(() => {
+    if (isButtonClicked) {
+      fetchBotMessage();
+      setIsButtonClicked(false);
+      setUserMessage("");
+    }
+  }, [isButtonClicked, fetchBotMessage]);
+
+  useEffect(() => {
+    if (chatContainerRef.current) {
+      chatContainerRef.current.scrollTop =
+        chatContainerRef.current.scrollHeight;
+    }
+  }, [chatMessages]);
+
+  const formatMessage = (message) => {
+    return message.split("```").map((snippet, index) => {
+      if (index % 2 === 1) {
+        return <CodeFormatter key={index} snippet={snippet} />;
+      } else {
+        return (
+          <span key={index}>
+            {snippet.split("\n").map((line, i) => (
+              <React.Fragment key={i}>
+                {line}
+                <br />
+              </React.Fragment>
+            ))}
+          </span>
+        );
+      }
+    });
+  };
+
+  const clearChat = () => {
+    setChatMessages([]);
+  };
+
+  const theme = getTheme();
+  const classNames = mergeStyleSets({
+    title: {
+      fontWeight: "bold",
+      fontSize: "2rem",
+      color: theme.palette.themePrimary
+    },
+    inputContainer: {
+      width: "100%",
+      display: "flex",
+      alignItems: "center"
+    },
+    inputBox: {
+      flexGrow: 1,
+      marginRight: "8px",
+      borderRadius: "25px",
+      border: "2px solid",
+      borderColor: theme.palette.themePrimary,
+      paddingLeft: "12px",
+      paddingRight: "12px",
+      paddingTop: "4px",
+      paddingBottom: "4px",
+      // backgroundColor: theme.palette.neutralLighter,
+      ":hover": {
+        borderColor: theme.palette.themeDark
+      },
+      ":focus-within": {
+        borderColor: theme.palette.themeDark
+      }
+    }
+  });
+
+  return (
+    <Layout>
+      {/* <Text className={classNames.title}>Ultron</Text> */}
+      <MainLayout
+        style={{
+          width: "100%"
+        }}
+      />
+
+      <Stack.Item>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            height: "70vh",
+            overflow: "auto"
+          }}
+          ref={chatContainerRef}
+        >
+          {chatMessages.map((message, index) => (
+            <div
+              key={index}
+              style={{
+                display: "flex",
+                justifyContent: message.isBot ? "flex-start" : "flex-end",
+                marginBottom: "8px"
+              }}
+            >
+              {message.isBot ? (
+                <BotMessage message={formatMessage(message.message)} />
+              ) : (
+                <UserMessage message={formatMessage(message.message)} />
+              )}
+            </div>
+          ))}
+        </div>
+      </Stack.Item>
+
+      <Stack horizontal gap={15} styles={{ root: { width: "100%" } }}>
+        <Icon
+          icon="game-icons:magic-broom"
+          style={{
+            cursor: "pointer",
+            fontSize: "1.5rem",
+            color: theme.palette.redDark,
+            alignSelf: "center"
+          }}
+          onClick={clearChat}
+        />
+        <TextField
+          className={classNames.inputBox}
+          value={userMessage}
+          onChange={handleUserMessageChange}
+          autoComplete="off"
+          autoFocus
+          multiline
+          resizable={false}
+          underlined
+          placeholder="Type..."
+        />
+        <Icon
+          icon="fluent:send-16-regular"
+          style={{
+            cursor: "pointer",
+            fontSize: "1.5rem",
+            color: theme.palette.themePrimary,
+            alignSelf: "center"
+          }}
+          onClick={handleSendMessage}
+        />
+      </Stack>
+    </Layout>
+  );
+};
+
+export default Chat;
---
+++ src/Components/UploadDocument.js
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import { Button } from "@material-ui/core";
+import CloudUploadIcon from "@material-ui/icons/CloudUpload";
+import UploadFileIcon from "@mui/icons-material/UploadFile";
+
+const SERVER_URL = "https://monkfish-app-vxijg.ondigitalocean.app";
+
+const UploadDocument = ({ onDocumentDataReceived }) => {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleUpload = async () => {
+    if (!selectedFile) return;
+
+    const formData = new FormData();
+    formData.append("file", selectedFile);
+
+    try {
+      const response = await fetch(`${SERVER_URL}/upload`, {
+        method: "POST",
+        body: formData
+      });
+
+      const data = await response.json();
+      console.log(data);
+      onDocumentDataReceived(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <>
+      <input
+        type="file"
+        accept=".txt,.pdf,.docx,.png,.jpg,.jpeg"
+        onChange={handleFileChange}
+        style={{ display: "none" }}
+        id="fileInput"
+      />
+      <label htmlFor="fileInput">
+        <Button component="span" color="primary" style={{ padding: 0 }}>
+          <UploadFileIcon fontSize="large" />
+        </Button>
+      </label>
+
+      {selectedFile && (
+        <Button
+          color="primary"
+          onClick={handleUpload}
+          style={{ marginLeft: "0px", padding: 0 }}
+        >
+          <CloudUploadIcon fontSize="large" />
+        </Button>
+      )}
+    </>
+  );
+};
+
+export default UploadDocument;
