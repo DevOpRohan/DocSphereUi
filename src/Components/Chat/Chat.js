40eabd6dc3c86eb36091d039818b1a5ae5ab00ca
Rohan Verma <86923317+DevOpRohan@users.noreply.github.com>
2023-07-04 16:28:43 +0530

    Update Chat.js

--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -37,7 +37,7 @@ const Chat = () => {
 
   const fetchBotMessage = useCallback(() => {
     fetch(
-      `https://5c9c-54-80-185-234.ngrok-free.app/get_answer/?question=${userMessage} && k=2`,
+      `https://6f26-54-80-185-234.ngrok-free.app/get_answer/?question=${userMessage} && k=2`,
       {
         headers: {
           "ngrok-skip-browser-warning": "1",
@@ -67,7 +67,7 @@ const Chat = () => {
           : Object.entries(groupedLinks).map(([link, pages], index) => (
               <a
                 key={index}
-                href={`https://5c9c-54-80-185-234.ngrok-free.app/?file=${link}`}
+                href={`https://6f26-54-80-185-234.ngrok-free.app/?file=${link}`}
                 target="_blank"
                 rel="noopener noreferrer"
                 style={{ marginRight: "8px" }}
